c989dd3fd7949643b4dfb95d66abc4f28b0aa8a8
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-10-19 20:49:42 +0300

    TASK 2 - Half DONE (четный поиск)

---
+++ src/mytasks/evennumbers/SolutionWorks.java
@@ -0,0 +1,63 @@
+package mytasks.evennumbers;
+
+import java.util.Scanner;
+
+public class SolutionWorks {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arrInput = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(arrInput[0]);          // k <= n
+        int k = Integer.parseInt(arrInput[1]);          //
+
+        String[] arr = scanner.nextLine().split(" ");
+        int[] nums = new int[n];
+
+        int countChots = 0;  // сколько четных значений
+        int firstSkips = 0;  // сколько первых элементов можно скипнуть
+        boolean firstBlock = true;
+        for (int i = 0; i < nums.length; i++) {
+            int currentInt = Integer.parseInt(arr[i]) % 2;
+            nums[i] = currentInt;
+            if (currentInt == 0 ){
+                countChots++;
+            } else if (firstSkips == i){ // тут нечетные
+                firstSkips++;
+            }
+        }
+        int max = 0;
+        for (int i = firstSkips; i < n; i++) {
+            int newMax = find(nums, k, firstSkips, countChots);
+            firstSkips++;
+            max = Math.max(max, newMax);
+        }
+        System.out.println(max);
+
+    }
+
+    static int find(int[] nums, int k, int firstSkips, int countChots){
+
+//        firstSkips сколько элемнтов можно скипнуть сначала
+        int maxLenght = 0;
+        int skips = 0;
+
+        for (int i = firstSkips; i < nums.length; i++) {
+            int currentInt = nums[i];
+            if (maxLenght == countChots){
+                return maxLenght;
+            }
+            if (currentInt == 0){
+                maxLenght++;
+            } else if (k != 0){
+                k--;
+                maxLenght++;
+            } else {
+                return maxLenght;
+            }
+        }
+
+        return maxLenght;
+    }
+}
+

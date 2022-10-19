7388e16cb58f7cb1cca73cac6da92fdc165ffc97
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-10-19 08:50:41 +0300

    TASK 5 - DONE

---
+++ src/mytasks/attractions/Solution2.java
@@ -0,0 +1,17 @@
+package mytasks.attractions;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+        Map<int[], Integer> map = new TreeMap<>(Arrays::compare);
+
+
+
+
+    }
+}
